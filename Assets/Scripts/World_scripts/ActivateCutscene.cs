b468675d7e14338347047fae7f541b94f189b1ed
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-07-08 17:05:09 +0200

    cutscene

---
+++ Assets/Scripts/World_scripts/ActivateCutscene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class ActivateCutscene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector playableDirector;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            playableDirector.Play();
+            GetComponent<BoxCollider2D>().enabled = false;
+            playableDirector.stopped += OnCutsceneEnd; // Prijavite se na događaj završetka cutscene-a
+        }
+    }
+
+    private void OnCutsceneEnd(PlayableDirector director)
+    {
+        SceneManager.LoadScene("bossFight"); // Učitava scenu naziva "FinalLevel"
+    }
+}
